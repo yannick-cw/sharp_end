c12fc504b2166969f67b7f22168f7573c649208b
Yannick Gladow <yannick.gladow@holidaycheck.com>
2019-10-21 10:16:35 +0200

    Remove rss feed plug

--- gatsby-config.js
+++ gatsby-config.js
@@ -223,67 +223,6 @@ module.exports = {
       }
     },
     {
-      resolve: `gatsby-plugin-feed`,
-      options: {
-        query: `
-          {
-            site {
-              siteMetadata {
-                title
-                description
-                siteUrl
-                site_url: siteUrl
-              }
-            }
-          }
-        `,
-        feeds: [
-          {
-            serialize: ({ query: { site, allMarkdownRemark } }) => {
-              return allMarkdownRemark.edges.map(edge => {
-                return Object.assign({}, edge.node.frontmatter, {
-                  description: edge.node.excerpt,
-                  date: edge.node.fields.prefix,
-                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
-                  guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
-                  custom_elements: [{ "content:encoded": edge.node.html }]
-                });
-              });
-            },
-            query: `
-              {
-                allMarkdownRemark(
-                  limit: 1000,
-                  sort: { order: DESC, fields: [fields___prefix] },
-                  filter: {
-                    fields: {
-                      prefix: { regex: "/[0-9]{4}.*/" },
-                      slug: { ne: null }
-                    }
-                  }
-                ) {
-                  edges {
-                    node {
-                      excerpt
-                      html
-                      fields {
-                        slug
-                        prefix
-                      }
-                      frontmatter {
-                        title
-                      }
-                    }
-                  }
-                }
-              }
-            `,
-            output: "/rss.xml"
-          }
-        ]
-      }
-    },
-    {
       resolve: `gatsby-plugin-sitemap`
     },
     {
